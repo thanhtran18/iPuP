d16cd95d656e16f7675fd756130ca36d176fe6bc
amanjyot <amanjyot2911@gmail.com>
2018-01-23 16:23:52 -0600

    Refactored the name to iPuP

---
+++ app/src/main/java/comp3350/iPuP/business/AccessCourses.java
@@ -0,0 +1,85 @@
+package comp3350.iPuP.business;
+
+import java.util.List;
+
+import comp3350.iPuP.application.Main;
+import comp3350.iPuP.application.Services;
+import comp3350.iPuP.objects.Course;
+import comp3350.iPuP.persistence.DataAccessStub;
+
+public class AccessCourses
+{
+	private DataAccessStub dataAccess;
+	private List<Course> courses;
+	private Course course;
+	private int currentCourse;
+
+	public AccessCourses()
+	{
+		dataAccess = (DataAccessStub) Services.getDataAccess(Main.dbName);
+		courses = null;
+		course = null;
+		currentCourse = 0;
+	}
+
+    public String getCourses(List<Course> courses)
+    {
+        courses.clear();
+        return dataAccess.getCourseSequential(courses);
+    }
+
+	public Course getSequential()
+	{
+		String result = null;
+		if (courses == null)
+		{
+            result = dataAccess.getCourseSequential(courses);
+			currentCourse = 0;
+		}
+		if (currentCourse < courses.size())
+		{
+			course = (Course) courses.get(currentCourse);
+			currentCourse++;
+		}
+		else
+		{
+			courses = null;
+			course = null;
+			currentCourse = 0;
+		}
+		return course;
+	}
+
+	public Course getRandom(String courseID)
+	{
+		courses = dataAccess.getCourseRandom(new Course(courseID));
+		currentCourse = 0;
+		if (currentCourse < courses.size())
+		{
+			course = (Course) courses.get(currentCourse);
+			currentCourse++;
+		}
+		else
+		{
+			courses = null;
+			course = null;
+			currentCourse = 0;
+		}
+		return course;
+	}
+
+	public String insertCourse(Course currentCourse)
+	{
+		return dataAccess.insertCourse(currentCourse);
+	}
+
+	public String updateCourse(Course currentCourse)
+	{
+		return dataAccess.updateCourse(currentCourse);
+	}
+
+	public String deleteCourse(Course currentCourse)
+	{
+		return dataAccess.deleteCourse(currentCourse);
+	}
+}

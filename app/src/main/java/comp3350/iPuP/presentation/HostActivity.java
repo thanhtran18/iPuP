c8dac537cc4697c97aa98904c909ff03b7c77a9f
amanjyot <amanjyot2911@gmail.com>
2018-01-26 03:26:36 -0600

    HostActivity.java: - added code to save the info of parking spot in the object and display   a toast if it was successful or if it failed - added the rate variable to properly create the new ParkingSpot object   but Mark will need to get the rate from the user via UI.

--- app/src/main/java/comp3350/iPuP/presentation/HostActivity.java
+++ app/src/main/java/comp3350/iPuP/presentation/HostActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -15,16 +16,19 @@ import java.util.Calendar;
 import java.util.Date;
 
 import comp3350.iPuP.R;
+import comp3350.iPuP.objects.ParkingSpot;
+import comp3350.iPuP.business.AccessParkingSpots;
 
 public class HostActivity extends Activity
 {
-
+    private AccessParkingSpots accessParkingSpots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
+        accessParkingSpots = new AccessParkingSpots();
     }
 
     @TargetApi(Build.VERSION_CODES.O)
@@ -38,6 +42,7 @@ public class HostActivity extends Activity
         String email = edit.getText().toString();
         edit =  (EditText) findViewById(R.id.editPhone);
         String phone = edit.getText().toString();
+        Double rate = 0.0;
         DatePicker datePicker =  (DatePicker) findViewById(R.id.datePickerDate);
         int day = datePicker.getDayOfMonth();
         int month = datePicker.getMonth();
@@ -50,6 +55,18 @@ public class HostActivity extends Activity
         hour = timePickerEnd.getHour();
         minute = timePickerEnd.getMinute();
         LocalDateTime end = LocalDateTime.of(year, month+1, day+1, hour, minute);
+
+        ParkingSpot newParkingSpot = new ParkingSpot(start,end,address,name,phone,email,rate);
+        String rtn = accessParkingSpots.insertParkingSpot(newParkingSpot);
+
+        if (rtn == null)
+        {
+            Toast.makeText(this, "New advertisement created!", Toast.LENGTH_LONG).show();
+        } else
+        {
+            Toast.makeText(this, "Failed to create new advertisement!", Toast.LENGTH_LONG).show();
+        }
+
         finish();
     }
 }

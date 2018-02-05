cd7469d5b2a86ac4f6744457381871b64c5c64cc
kram1138 <kram1138@hotmail.com>
2018-02-05 17:05:53 -0600

    Changed LocalDateTime object in ParkingSpot class to a custom ReservationTime object.

---
+++ app/src/main/java/comp3350/iPuP/objects/ReservationTime.java
@@ -0,0 +1,34 @@
+package comp3350.iPuP.objects;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Mark Van Egmond on 2/5/2018.
+ */
+
+public class ReservationTime
+{
+    private Date start;
+    private Date end;
+    private SimpleDateFormat date;
+    private SimpleDateFormat time;
+    public ReservationTime(int year, int month, int day, int startHour, int startMinute, int endHour, int endMinute)
+    {
+        Calendar c = new GregorianCalendar(year, month, day, startHour, startMinute);
+        date = new SimpleDateFormat("EEE, d MMM yyyy");
+        time = new SimpleDateFormat("h:mm");
+        start =  c.getTime();
+        c.set(year, month, day, endHour, endMinute);
+        end = c.getTime();
+    }
+
+    @Override
+    public String toString()
+    {
+        return date.format(start) + ", " + time.format(start) + " - " + time.format(end);
+    }
+}

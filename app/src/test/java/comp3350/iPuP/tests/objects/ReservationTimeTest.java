b31c538ad4c30217bfbecbd8fc8e956c7efb48e1
kram1138 <kram1138@hotmail.com>
2018-02-26 12:18:23 -0600

    Changed ReservationTime to use both a start and end date

--- app/src/test/java/comp3350/iPuP/tests/objects/ReservationTimeTest.java
+++ app/src/test/java/comp3350/iPuP/tests/objects/ReservationTimeTest.java
@@ -18,15 +18,10 @@ public class ReservationTimeTest extends TestCase
 
     public void testReservationTimes()
     {
-        assertTrue("Tue, 4 Apr 2017, 8:30 - 9:30".equals((new ReservationTime(2017,3, 4, 8,30,9,30)).toString()));
-        assertFalse((new ReservationTime(2017,3, 4, 8,30,9,30)).equals(new ReservationTime(2018,3, 4, 8,30,9,30)));
-        assertFalse((new ReservationTime(2017,3, 4, 8,30,9,30)).equals(null));
-        assertFalse((new ReservationTime(2017,3, 4, 8,30,9,30)).equals(this));
+        ReservationTime time = new ReservationTime(2017,3, 4, 8,30,2017,3, 4,9,30, false);
+        assertEquals("Tue, 4 Apr 2017, 8:30 AM - Tue, 4 Apr 2017, 9:30 AM",time.toString());
 
-        assertTrue((new ReservationTime(2017,3, 4, 8,30,8,30)).getStart().equals((new GregorianCalendar(2017,3, 4, 8,30).getTime())));
-
-        assertTrue((new ReservationTime(2017,3, 4, 8,30,9,30)).getEnd().equals((new GregorianCalendar(2017,3, 4, 9,30).getTime())));
-
-        assertTrue("Invalid date".equals((new ReservationTime(0,0, 0, 0,0,0,0)).toString()));
+        time = new ReservationTime(2017,3, 4, 8,30,2017,3, 4,9,30, false);
+        try { assertEquals(time, ReservationTime.parseString("Tue, 4 Apr 2017, 8:30 AM - Tue, 4 Apr 2017, 9:30 AM", false));} catch(Exception e){}
     }
 }

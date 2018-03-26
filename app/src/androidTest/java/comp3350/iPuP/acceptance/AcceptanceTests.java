37796c77f36124826d99d5577fbd07a772fafaa0
thanhtran18 <tranct@myumanitoba.ca>
2018-03-26 16:01:14 -0500

    Added new test classes

--- app/src/androidTest/java/comp3350/iPuP/acceptance/AcceptanceTests.java
+++ app/src/androidTest/java/comp3350/iPuP/acceptance/AcceptanceTests.java
@@ -1,8 +1,20 @@
 package comp3350.iPuP.acceptance;
 
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
 /**
  * Created by ThanhTran on 2018-03-26.
  */
 
-public class AcceptanceTests {
+public class AcceptanceTests
+{
+    public static TestSuite suite;
+
+    public static Test suite()
+    {
+        suite = new TestSuite("Acceptance tests");
+        //suite.addTestSuite(StudentsTest.class);
+        return suite;
+    }
 }
---
+++ app/src/androidTest/java/comp3350/iPuP/acceptance/BookParkingSpotTest.java
@@ -0,0 +1,14 @@
+package comp3350.iPuP.acceptance;
+
+import junit.framework.Assert;
+
+import com.robotium.solo.Solo;
+
+/**
+ * Created by ThanhTran on 2018-03-26.
+ */
+
+public class BookParkingSpotTest
+{
+
+}

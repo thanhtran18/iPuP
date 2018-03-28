3be2379b3bdbf239baa6f3dbc42408af7a909df7
thanhtran18 <tranct@myumanitoba.ca>
2018-03-28 00:20:25 -0500

    Updated the integration tests with create booking method

--- app/src/test/java/comp3350/iPuP/tests/integration/BusinessPersistenceSeamTest.java
+++ app/src/test/java/comp3350/iPuP/tests/integration/BusinessPersistenceSeamTest.java
@@ -282,7 +282,7 @@ public class BusinessPersistenceSeamTest extends TestCase
         closeDataAccess();
         System.out.println("Finished testBusinessPersistenceSeam: Book A ParkingSpot");
     }
-    
+
     public void testDeleteBookedSpotsInHistory()
     {
         openDataAccess();
